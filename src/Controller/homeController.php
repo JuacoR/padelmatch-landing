b7cff280d8629e4584eef13bbe6c7e735e983cdf
Joaquin Ruatta <65560868+JuacoR@users.noreply.github.com>
2023-08-28 19:48:26 -0300

    test2

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class homeController extends AbstractController
 {
     /**
      * @Route("/home", name="app_home")
