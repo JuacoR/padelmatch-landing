54e2a68fce3c975fdda3a2a8a9f7387b9ffcd9d8
Joaquin Ruatta <65560868+JuacoR@users.noreply.github.com>
2023-08-28 19:46:36 -0300

    Tests

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,10 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(): BinaryFileResponse
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $filePath = $this->getParameter('kernel.project_dir') . '/public/index.html';
+
+        return new BinaryFileResponse($filePath);
     }
 }
